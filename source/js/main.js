5903d98a1c18963b5202e9e74b85a352bdde1193
Misha074 <103427594+Misha074@users.noreply.github.com>
2023-08-22 19:51:29 +0400

    Исправления
    Повторная проверка

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,4 @@
 import {iosVhFix} from './utils/ios-vh-fix';
-import {initModals} from './modules/modals/init-modals';
-import {Form} from './modules/form-validate/form';
 import {initHeroSwiper} from './modules/swiper-hero/swiper-hero';
 import './modules/youtube-video/youtube-video';
 import './modules/nav-toggle/nav-toggle';
@@ -11,6 +9,7 @@ import {initAdvantagesSwiper} from './modules/swiper-advantages/swiper-advantage
 import {initGallerySwiper} from './modules/swiper-gallery/swiper-gallery';
 import {inputValidate} from './modules/input-validate/input-validate';
 import {initLeaflet} from './modules/leaflet/leaflet';
+import './modules/scroll/scroll';
 // ---------------------------------
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -34,35 +33,5 @@ window.addEventListener('DOMContentLoaded', () => {
     initGallerySwiper();
     inputValidate();
     initLeaflet();
-
-    initModals();
-    const form = new Form();
-    window.form = form;
-    form.init();
   });
 });
-
-// ---------------------------------
-
-// ❗❗❗ обязательно установите плагины eslint, stylelint, editorconfig в редактор кода.
-
-// привязывайте js не на классы, а на дата атрибуты (data-validate)
-
-// вместо модификаторов .block--active используем утилитарные классы
-// .is-active || .is-open || .is-invalid и прочие (обязателен нейминг в два слова)
-// .select.select--opened ❌ ---> [data-select].is-open ✅
-
-// выносим все в дата атрибуты
-// url до иконок пинов карты, настройки автопрокрутки слайдера, url к json и т.д.
-
-// для адаптивного JS используется matchMedia и addListener
-// const breakpoint = window.matchMedia(`(min-width:1024px)`);
-// const breakpointChecker = () => {
-//   if (breakpoint.matches) {
-//   } else {
-//   }
-// };
-// breakpoint.addListener(breakpointChecker);
-// breakpointChecker();
-
-// используйте .closest(el)
--- source/js/modules/input-validate/input-validate.js
+++ source/js/modules/input-validate/input-validate.js
@@ -2,8 +2,6 @@ let questionsForm = document.querySelector('.questions__form');
 let inputName = questionsForm.querySelector('[type="text"]');
 let inputTel = questionsForm.querySelector('[type="tel"]');
 let submitButton = questionsForm.querySelector('[type="submit"]');
-let spanName = questionsForm.querySelector('.questions__span--name');
-let spanTel = questionsForm.querySelector('.questions__span--tel');
 
 function resetInputs() {
   inputName.value = '';
@@ -14,18 +12,14 @@ function inputValidate() {
   submitButton.addEventListener('click', () => {
     if (!inputName.value) {
       inputName.classList.add('questions__input--invalid');
-      spanName.style.display = 'block';
     } else {
       inputName.classList.remove('questions__input--invalid');
-      spanName.style.display = 'none';
     }
 
     if (!inputTel.value) {
       inputTel.classList.add('questions__input--invalid');
-      spanTel.style.display = 'block';
     } else {
       inputTel.classList.remove('questions__input--invalid');
-      spanTel.style.display = 'none';
     }
 
     if ((!inputName.value) || (!inputTel.value)) {
@@ -43,10 +37,8 @@ function inputValidate() {
     inputName.value = inputName.value.replace(/[^a-zA-Zа-яА-Я,' ',-]/g, '');
     if (inputName.value.length < 1) {
       inputName.classList.add('questions__input--invalid');
-      spanName.style.display = 'block';
     } else {
       inputName.classList.remove('questions__input--invalid');
-      spanName.style.display = 'none';
     }
   };
 
@@ -54,10 +46,8 @@ function inputValidate() {
     inputTel.value = inputTel.value.replace(/[^0-9,+,(),-]/g, '');
     if (inputTel.value.length < 1) {
       inputTel.classList.add('questions__input--invalid');
-      spanTel.style.display = 'block';
     } else {
       inputTel.classList.remove('questions__input--invalid');
-      spanTel.style.display = 'none';
     }
   };
 }
